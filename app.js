aed72e9ade875bb20fc98696086596ebdbc97a0d
AlfredEngberg <alfred.engberg@elev.ga.ntig.se>
2023-08-31 11:01:14 +0200

    ✨ variealver kod och sånt

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log("yooo whats up");
+
+let firstname = 'affe';
+let lastname = 'engberg';
+let num = 94;
+console.log(typeof(num));
+
+var NAME = 'Stina';
+
+console.log(NAME);
+
+for (let i = 0; i < 10; i++){
+var NAME = "Sture";
+// NAME = "L (FRÅN FAMLT GIY"; DETTA GÅR INTE FÖR ATT DET ÄR ÄR CONST
+console.log(NAME); 
+}
+
+var skola = "NTI GYMNASIET I UMEÅ";
+console.log(skola);
+
+let a = "Affe"; // String
+let b = 'Affe'; // Stirng
+let c = `Mitt namn är ${a}`; // String med template literals
+let d = 'Mitt namn är ' + b; // Kontaktenering
+let e = 12; // num 
+let f = 16.7; // num 
+let g = true; // boolean
+let i = false; 
+
+
+console.log(c, e, g);
